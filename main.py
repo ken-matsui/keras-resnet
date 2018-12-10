f765da77a046f7a13a4dc3b765ce44abe2810dbc
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-12-10 19:05:59 +0900

    fix err

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main(_):
     train_datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=True,  # divide inputs by std of the dataset
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
         zca_whitening=False,  # apply ZCA whitening
         rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
