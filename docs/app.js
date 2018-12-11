0f9655c6fcaf20b08b5b19897dc547e6ef786c70
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-12-12 01:16:29 +0900

    Create docs

---
+++ docs/app.js
@@ -0,0 +1,30 @@
+// load pre-trained model
+let model;
+tf.loadModel('./model/model.json')
+    .then(pretrainedModel => {
+        document.getElementById("inference").removeAttribute("disabled");
+        model = pretrainedModel;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+function getResponseSentence(imageData) {
+    const score = tf.tidy(() => {
+        // convert to tensor (shape: [width, height, channels])
+        const channels = 1; // grayscale
+        let input = tf.fromPixels(imageData, channels);
+        // normalized
+        input = tf.cast(input, 'float32').div(tf.scalar(255));
+        // reshape input format (shape: [batch_size, width, height, channels])
+        input = input.expandDims();
+        // predict
+        return model.predict(input).dataSync();
+    });
+    return score;
+}
+
+function inference() {
+    console.log(document.getElementById("input_text").value);
+    // getResponseSentence();
+}
